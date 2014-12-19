46c836a5ff91e9f1e1a0ea74652fb1397fc3dc72
Lorenzo Braghetto <monossido@lorenzobraghetto.com>
2014-12-19 15:45:26 +0100

    added seeds.rb example in order to populate Ghexe data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Ghexe example in order to populate data, Create your Users first
+#Event.create(title: 'Pranzo', dayofweek: 0, time: '13:00')
+#Event.create(title: 'Cena', dayofweek: 0, time: '20:00')
+#Event.create(title: 'Pranzo', dayofweek: 1, time: '13:00')
+#Event.create(title: 'Cena', dayofweek: 1, time: '20:00')
+#Event.create(title: 'Pranzo', dayofweek: 2, time: '13:00')
+#Event.create(title: 'Cena', dayofweek: 2, time: '20:00')
+#Event.create(title: 'Pranzo', dayofweek: 3, time: '13:00')
+#Event.create(title: 'Cena', dayofweek: 3, time: '20:00')
+#Event.create(title: 'Pranzo', dayofweek: 4, time: '13:00')
+#Event.create(title: 'Cena', dayofweek: 4, time: '20:00')
+#Event.create(title: 'Pranzo', dayofweek: 5, time: '13:00')
+#Event.create(title: 'Cena', dayofweek: 5, time: '20:00')
+#Event.create(title: 'Pranzo', dayofweek: 6, time: '13:00')
+#Event.create(title: 'Cena', dayofweek: 6, time: '20:00')
+
+#User.find_each do |u|
+#  Event.find_each do |e|
+#    Presence.create(user_id: u.id, event_id: e.id, presence: true)
+#  end
+#end

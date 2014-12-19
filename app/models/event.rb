2a269808b5da3fd684b63ec17100212deaa07587
Lorenzo Braghetto <monossido@lorenzobraghetto.com>
2014-12-19 15:46:00 +0100

    Deleting Associated Objects model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  has_many :presences
+  has_many :presences, dependent: :destroy
   validates :title, presence: true, length: { minimum: 3 }
   validates :dayofweek, presence: true
   validates :time, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :presences
+  has_many :presences, dependent: :destroy
 end
